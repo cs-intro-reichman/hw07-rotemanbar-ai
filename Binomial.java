f35e7aad70d4a6ee45f609e112f5eed272234589
Rotem Anbar <rotemanbar@gmail.com>
2025-12-26 23:59:54 +0200

    Fixed missing curly brace in Binomial

--- Binomial.java
+++ Binomial.java
@@ -1,51 +1,35 @@
-/** Computes the binomial(n,k) function. */
-public class Binomial {	
+public class Binomial {
     public static void main(String[] args) {
         if (args.length < 2) return;
-           int n = Integer.parseInt(args[0]);
-           int k = Integer.parseInt(args[1]);
-	}
+        int n = Integer.parseInt(args[0]);
+        int k = Integer.parseInt(args[1]);
+        System.out.println(binomial(n, k));
+    }
 
-	// Computes the Binomial function, basic version.
-	public static int binomial1(int n, int k) { 
-		if (k > n) {
-            return 0;
-        }
-		if (k == 0 || n == 0) {
-            return 1;
-        }
-		return binomial1(n - 1, k) + binomial1(n - 1, k - 1);
-	 }
-	
-	// Computes the Binomial function, efficiently
-	public static int binomial(int n, int k) {	
-	    int[][] memo = new int[n + 1][k + 1];
-	    for (int i = 0; i <= n; i++) {
-            for (int j = 0; j <= k; j++) {
-                memo[i][j] = -1;
-            }
-        }
-	    return binomial(n, k, memo);
-	}
+    public static long binomial1(int n, int k) {
+        if (k > n) return 0;
+        if (k == 0 || k == n) return 1;
+        return binomial1(n - 1, k) + binomial1(n - 1, k - 1);
+    }
 
-	private static int binomial(int n, int k, int[][] memo) {
-		if (memo[n][k] != -1) {
-			return memo[n][k];
-		}
-		// Base case
-		if ((k > n)) {
-		   	memo[n][k] = 0; 
-		   	return 0;
-		}
-		// Another base case
-		if (n == 0 || k == 0) {
-		   	memo[n][k] = 1; 
-		   	return 1;
-		}
-		memo[n][k] = binomial(n - 1, k, memo) + binomial(n - 1, k - 1, memo);
-		return memo[n][k];
-	}
+    public static long binomial(int n, int k) {
+        if (k > n) return 0;
+        if (k == 0 || k == n) return 1;
+        if (k > n / 2) k = n - k;
+        
+        long[][] memo = new long[n + 1][k + 1];
+        return binomial(n, k, memo);
+    }
+
+    private static long binomial(int n, int k, long[][] memo) {
+        if (k < 0 || k > n) return 0;
+        if (k == 0 || k == n) return 1;
+        
+        if (memo[n][k] != 0) {
+            return memo[n][k];
+        }
+        
+        memo[n][k] = binomial(n - 1, k, memo) + binomial(n - 1, k - 1, memo);
+        return memo[n][k];
+    }
 }
-
-
-
